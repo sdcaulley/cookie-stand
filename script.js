a998da9462af01b85a5abc23f6a845f2be10204f
sdcaulley <stephanie.caulley@gmail.com>
2016-07-14 07:28:40 -0700

    objects created

--- script.js
+++ script.js
@@ -1,9 +1,79 @@
-var averageCustomer = [];
-
-function avgCustGen(min, max) {
-while (averageCustomer.length < 10) {
-  var randomCust = (Math.floor(Math.random() * (max - min) + min));
-  averageCustomer.push(randomCust);
-  console.log(averageCustomer);
+var store = {
+  name: 'Pioneer Square';
+  minCustomer: 17;
+  maxCustomer: 88;
+  avgCookie: 5.2;
+  randomCust: [];
+  cookiesNeeded: function (minCustomer, maxCustomer, avgCookie) {
+    while (randomCust.length < 10) {
+      var cookieSales = ((Math.floor(Math.random() * (maxCustomer - minCustomer) + minCustomer)) * avgCookie);
+    randomCust.push(cookieSales);
+    console.log(cookieSales);
+    console.log(randomCust);
+    }
+  }
 }
+
+var store = {
+  name: 'Portland Airport';
+  minCustomer: 6;
+  maxCustomer: 24;
+  avgCookie: 1.2;
+  randomCust: [];
+  cookiesNeeded: function (minCustomer, maxCustomer, avgCookie) {
+    while (randomCust.length < 10) {
+      var cookieSales = ((Math.floor(Math.random() * (maxCustomer - minCustomer) + minCustomer)) * avgCookie);
+    randomCust.push(cookieSales);
+    console.log(cookieSales);
+    console.log(randomCust);
+    }
+  }
+}
+
+var store = {
+  name: 'Washington Square';
+  minCustomer: 11;
+  maxCustomer: 38;
+  avgCookie: 1.9;
+  randomCust: [];
+  cookiesNeeded: function (minCustomer, maxCustomer, avgCookie) {
+    while (randomCust.length < 10) {
+      var cookieSales = ((Math.floor(Math.random() * (maxCustomer - minCustomer) + minCustomer)) * avgCookie);
+    randomCust.push(cookieSales);
+    console.log(cookieSales);
+    console.log(randomCust);
+    }
+  }
+}
+
+var store = {
+  name: 'Sellwood';
+  minCustomer: 20;
+  maxCustomer: 48;
+  avgCookie: 3.3;
+  randomCust: [];
+  cookiesNeeded: function (minCustomer, maxCustomer, avgCookie) {
+    while (randomCust.length < 10) {
+      var cookieSales = ((Math.floor(Math.random() * (maxCustomer - minCustomer) + minCustomer)) * avgCookie);
+    randomCust.push(cookieSales);
+    console.log(cookieSales);
+    console.log(randomCust);
+    }
+  }
+}
+
+var store = {
+  name: 'Pearl District';
+  minCustomer: 3;
+  maxCustomer: 24;
+  avgCookie: 2.6;
+  randomCust: [];
+  cookiesNeeded: function (minCustomer, maxCustomer, avgCookie) {
+    while (randomCust.length < 10) {
+      var cookieSales = ((Math.floor(Math.random() * (maxCustomer - minCustomer) + minCustomer)) * avgCookie);
+    randomCust.push(cookieSales);
+    console.log(cookieSales);
+    console.log(randomCust);
+    }
+  }
 }
